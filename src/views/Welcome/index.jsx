bbcef068cc14d442438495b83b01ed475e08d307
Sarah Conway Schnurr <sarah.conway@crunchydata.com>
2019-11-04 06:59:11 -0800

    Views: Restructure Welcome view (#9)
    - Add content & structure
    - Add CSS

--- src/views/Welcome/index.jsx
+++ src/views/Welcome/index.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
-import { PageSection } from '@patternfly/react-core';
+import {
+  Grid,
+  GridItem,
+  PageSection,
+  Text,
+  TextContent,
+} from '@patternfly/react-core';
 import { ReactComponent as Logo } from './images/hippos.svg';
+import styles from './index.module.css';
 
 const Welcome = () => (
   <PageSection>
-    <Logo />
+    <Grid gutter="md">
+      <GridItem span={6}>
+        <Logo />
+      </GridItem>
+      <GridItem span={6}>
+        <TextContent className={styles.pageHeader}>
+          <Text>
+            <strong>Welcome</strong> to the Santa Cruz County Wildfire Management Application!
+          </Text>
+        </TextContent>
+      </GridItem>
+    </Grid>
   </PageSection>
 );
 
