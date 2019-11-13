7cfdd830ef1ec840a8b1cc80068befdc30df5a68
Sarah Conway Schnurr <sarah.conway@crunchydata.com>
2019-11-13 12:11:45 -0800

    Add Emergency Alert popup

---
+++ src/views/Notification/components/NotificationAlert/index.jsx
@@ -0,0 +1,23 @@
+// @ts-check
+import React from 'react';
+import {
+  Alert,
+  AlertActionCloseButton,
+} from '@patternfly/react-core';
+import styles from './index.module.css';
+
+const NotificationAlert = props => {
+  const { onClose } = props;
+  return (
+    <Alert
+      className={styles.root}
+      variant="success"
+      title="Success alert title"
+      action={<AlertActionCloseButton onClose={onClose} />}
+    >
+      Homeowners have been notified!
+    </Alert>
+  );
+};
+
+export default NotificationAlert;
